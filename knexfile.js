ef4b48999aebe64b36b3514960424e3ae9b96fdf
rigo0523 <limon1285@gmail.com>
2022-05-19 17:59:43 -0500

    add production

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,20 @@ module.exports = {
       },
     },
   },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
 };
