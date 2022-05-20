6b284870de2ae32dd97d8fea5cb6c3a335a3e1f9
rigo0523 <limon1285@gmail.com>
2022-05-19 22:49:20 -0500

    refactor knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,27 @@
 require("dotenv").config();
-module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./database/listings.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-    pool: {
-      // for foreign keys only
-      afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-      },
+const sharedConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./database/migrations",
+  },
+  seeds: {
+    directory: "./database/seeds",
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
     },
   },
+};
 
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: { filename: "./database/reservations.db3" },
+  },
   production: {
-    client: "sqlite3",
-    connection: { filename: "./database/listings.db3" },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    ...sharedConfig,
+    connection: { filename: "./database/reservations.db3" },
   },
 };
