b1688e11891e19609ddc16038dfc4cd8d3f294f1
rigo0523 <limon1285@gmail.com>
2022-05-19 22:17:58 -0500

    heroku push

--- knexfile.js
+++ knexfile.js
@@ -21,19 +21,19 @@ module.exports = {
     },
   },
 
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./database/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
+  // production: {
+  //   client: "pg",
+  //   connection: process.env.DATABASE_URL,
+  //   migrations: {
+  //     directory: "./database/migrations",
+  //     tableName: "knex_migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./database/seeds",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  // },
 };
